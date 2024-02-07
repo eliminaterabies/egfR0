e2edee8f66af4ca282cc648b60905ccf3c6d993f
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-02-07 16:24:32 -0500

    modified:   rplot.R

--- rplot.R
+++ rplot.R
@@ -4,7 +4,9 @@ library(ggplot2); theme_set(theme_bw())
 library(shellpipes)
 startGraphics()
 
-## FIXME; what is a good way to order these factors?
+## FIXME: what is a good way to order these factors?
+## HINT: Do _not_ take the line below as a starting point; try the forcats package instead.
+## We should figure out how to make them appear forward alphabetically, but also how to order them by the mean value of r.
 dat <- (rdsRead()
 	## |> mutate(loc = factor(loc, levels=rev(levels(loc))))
 )
