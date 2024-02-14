75ce4f746fbb61481012a298f47d211933a4aba9
mikeroswell <mike.roswell@gmail.com>
2024-02-14 17:19:12 -0500

    added startGraphics() to R0combo.R to see new plot ordered by R0 estimates (with NA first, now, could be changed). I don't know how Mike uses the makefile to make stuff in the "outputs" directory... but that is probably something to revert.

--- R0combo.R
+++ R0combo.R
@@ -1,13 +1,21 @@
 library(ggplot2);theme_set(theme_bw())
 library(shellpipes)
+library(dplyr)
+startGraphics()
 
 gg <- rdsRead("plot")
 hampson <- rdsRead("KH")
 
 
 print(gg 
-	+ geom_linerange(data=hampson,aes(ymin=lower,ymax=upper),color="blue",size=3,alpha=0.2)
+	+ geom_linerange(data=hampson %>% mutate(est = med)
+	                 , aes(ymin=lower
+	                       , ymax=upper)
+	                 , color="blue"
+	                 , linewidth = 3
+	                 , alpha=0.2)
 	+ coord_flip())
 
 rdsSave(gg)
+
 
--- R0plot.R
+++ R0plot.R
@@ -4,21 +4,26 @@ library(ggplot2); theme_set(theme_bw())
 library(shellpipes)
 startGraphics()
 
-## FIXME: what is a good way to order these factors?
-## HINT: Do _not_ take the line below as a starting point; try the forcats package instead.
-## We should figure out how to make them appear forward alphabetically, but also how to order them by the mean value of r.
+
 dat <- (rdsRead()
 	## |> mutate(loc = factor(loc, levels=rev(levels(loc))))
 	## %>% filter(phase != 3) ## removing toyko phase 3s
 )
 
-gg <- (ggplot(dat)
-	+ aes(x=loc)
-	+ geom_pointrange(aes(ymin=lwr,ymax=upr,y=est,color=phase)
+print(dat)
+
+gg <- (ggplot(dat
+	, aes( x = forcats::fct_reorder(loc, est, .fun = mean, .na_rm = FALSE)
+	        )
+)
+	+ geom_pointrange(aes(ymin=lwr
+	                      , y = est
+	                      , ymax=upr
+	                      , color=phase)
 		, position = position_dodge(width=-0.4)
 	)
 	+ scale_color_manual(values=c("black","red", "orange"))
-	+ ylab("R0")
+	+ labs(y = "R0", x = NULL)
 )
 
 print(gg + coord_flip())
