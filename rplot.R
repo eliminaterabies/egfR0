8d0b507211ebb07a797701535b5151035e263186
mikeroswell <mike.roswell@gmail.com>
2024-02-14 16:28:04 -0500

    Autocommit

--- rplot.R
+++ rplot.R
@@ -1,18 +1,23 @@
 library(dplyr, warn.conflicts=FALSE)
 library(ggplot2); theme_set(theme_bw())
-
+library(forcats)
 library(shellpipes)
 startGraphics()
 
-## FIXME: what is a good way to order these factors?
-## HINT: Do _not_ take the line below as a starting point; try the forcats package instead.
-## We should figure out how to make them appear forward alphabetically, but also how to order them by the mean value of r.
 dat <- (rdsRead()
 	## |> mutate(loc = factor(loc, levels=rev(levels(loc))))
 )
 
-print(ggplot(dat)
-	+ aes(x=loc, y=est, color=phase)
+print(
+       ggplot(dat)
+	+ aes(
+	    # to simply reverse:
+	    # x=forcats::fct_rev(loc)
+	    # This is reordering by the first est for each loc:
+	    # x = forcats::fct_reorder(loc, est)
+	    # This is reordering by the mean est:
+	    x = forcats::fct_reorder(loc, est, .fun = mean)
+	    , y=est, color=phase)
 	+ geom_pointrange(aes(ymin=lwr,ymax=upr)
 		, position = position_dodge(width=-0.4)
 	)
