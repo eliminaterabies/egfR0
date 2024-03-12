6874c6582aa94840bad77d5ef1e85c6069b295ce
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-03-12 15:59:35 -0400

    Rabies screen

--- conflicts.R
+++ conflicts.R
@@ -9,7 +9,6 @@
 # another option is to use `options()` in base R to get desired behavior.
 # for reference, https://developer.r-project.org/Blog/public/2019/03/19/managing-search-path-conflicts/
 
-
 # a slightly more liberal version of the recommended recipe might be what we
 # want. I think this will silence the annoyingest warnings (e.g., where
 # individual packages from the tidyverse mask base functions) but keep other
@@ -17,26 +16,4 @@
 # loading `library(tidyverse)` triggers `tidyverse_conflicts()` to run, which
 # can be silenced manually.
 
-# The below is sufficient, but maybe more than necessary?
-
-# Now, my options
-
-## Over-ride base stuff silently (see “mask”). Make user resolve other conflicts
-options(
-	conflicts.policy = list(
-		error = TRUE, warn = FALSE, generics.ok = TRUE
-		, can.mask = c("base", "methods", "utils"
-			, "grDevices", "graphics", "stats"
-		)
-		, depends.ok = TRUE
-	)
-	, tidyverse.quiet = TRUE
-)
-
-library(tidyverse) # silent, by default
-# library(MASS) # throws an error, arguably desirable
-library(MASS, exclude = "select")
-message("I loaded MASS without error")
-# library(dplyr)
-# library(data.table) # again, with error that would need to be resolved
-# END DONT RUN
+## Content moved to makestuff/wrapR/conflicts.R
